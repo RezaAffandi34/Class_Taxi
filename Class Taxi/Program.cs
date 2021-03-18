eed33b2fe7569c9aacca05c1aa821c7939753f61
RezaAffandi34 <reza02affandi@gmail.com>
2021-03-18 20:52:55 +0700

    Class_Taxi

---
+++ Class Taxi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Taxi
+    {
+        public string DriverName;
+        public bool OnDuty;
+        public int NumPassenger;
+
+        static void Main(string[] args)
+        {
+            // membuat objek Taxi
+            Taxi taxi = new Taxi();
+
+            // pengesetan nilai properties
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            // pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOutPassenger();
+
+            Console.ReadKey();
+        }
+
+        
+       
+    }
+}
